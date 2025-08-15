759806c72ff3d99d0a920cba24974baf96737a30
Stanislav Chernyshev <madmasm@gmail.com>
2025-08-16 00:29:30 +0300

    del

--- part_9_pre-alpha/augment_class/test/augment_class_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:augment_class/augment_class.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
--- part_9_pre-alpha/macros_first_blood/test/macros_first_blood_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:macros_first_blood/macros_first_blood.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
